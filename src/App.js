4baea49ee4e248868337d40e78db800f2c28604e
VMattuz <valentin.mattuz@santa-ana.edu.ar>
2025-10-07 21:33:28 -0300

    Contenidop1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Menu from './components/Menu/Menu';
 import Header from "./components/Header/Header"
-import Footer from "./components/Footer/Footer"
 import Nosotros from './components/Aboutus/Nosotros';
+import Footer from "./components/Footer/Footer"
+
 
 function App() {
   return (
@@ -12,7 +13,6 @@ function App() {
     <Header />
       <Routes>
         <Route path="/" element={<Menu />} />
-        
         <Route path="/about" element={<Nosotros />} />
       </Routes>
     <Footer />
